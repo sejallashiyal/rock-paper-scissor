47ab10c06bb095e02cacccacd34d340449e08016
sejallashiyal <73558271+sejallashiyal@users.noreply.github.com>
2020-11-01 21:35:35 +0530

    Add files via upload

---
+++ modal.js
@@ -0,0 +1,17 @@
+const DOMStrings = {
+
+    rules: '.rules',
+    ruleBox: '.rules-overlay',
+    closeBtn: '.cross-btn',
+
+}
+
+//fade rules box
+document.querySelector(DOMStrings.rules).addEventListener('click', () => {
+    document.querySelector(DOMStrings.ruleBox).classList.add('active')
+})
+
+//close rules box
+document.querySelector(DOMStrings.closeBtn).addEventListener('click', () => {
+    document.querySelector(DOMStrings.ruleBox).classList.remove('active');
+})
